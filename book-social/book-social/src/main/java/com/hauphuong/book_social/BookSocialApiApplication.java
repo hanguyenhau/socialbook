b19bba2b3e4d9d7408ff007223355b1957195e18
haukutekontum <haukutekontum@gmail.com>
2024-06-01 16:27:34 +0700

    Set up docker and required env

---
+++ book-social/book-social/src/main/java/com/hauphuong/book_social/BookSocialApiApplication.java
@@ -0,0 +1,13 @@
+package com.hauphuong.book_social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSocialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSocialApiApplication.class, args);
+	}
+
+}
---
+++ book-social/book-social/src/test/java/com/hauphuong/book_social/BookSocialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hauphuong.book_social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSocialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
