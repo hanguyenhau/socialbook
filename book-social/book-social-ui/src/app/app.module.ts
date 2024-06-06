fb3257c5a0426116f02bfccb578603c18334921b
haukutekontum <haukutekontum@gmail.com>
2024-06-07 00:51:49 +0700

    Error: blocked by CORS policy still fixing

--- book-social/book-social-ui/src/app/app.module.ts
+++ book-social/book-social-ui/src/app/app.module.ts
@@ -4,17 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { LoginComponent } from './pages/login/login.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FaIconComponent
+    FaIconComponent,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    HttpClient
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
